e018857bf5497bbb278ed1a8c7a4bdc4330cb663
Gustavo Luz <gustavofernandezluz@gmail.com>
2025-04-10 20:43:53 -0300

    AR + visualizador web

---
+++ threejs/11/main.js
@@ -0,0 +1,198 @@
+import { ARButtonCustom } from './ARButtonCustom.js';
+
+Bugfender.init({
+  appKey: 'akOMxVZb98TxG0lOt8HWKxaEc9D4mqQU',
+});
+
+window.addEventListener('load', async () => {
+  const THREE = await import('three');
+  const GLTFLoaderModule = await import('three/addons/loaders/GLTFLoader.js');
+  const GLTFLoader = GLTFLoaderModule.GLTFLoader;
+  const OrbitControlsModule = await import('three/addons/controls/OrbitControls.js');
+  const OrbitControls = OrbitControlsModule.OrbitControls;
+
+  let ar_supported = false;
+
+  let container;
+  let camera, scene, renderer;
+  let controller;
+  let controls;
+
+  let reticle;
+
+  let hitTestSource = null;
+  let hitTestSourceRequested = false;
+  let planeFound = false;
+  let noteGltf;
+
+  // check for webxr session support
+  if ("xr" in navigator) {
+    console.log(navigator);
+
+    navigator.xr.isSessionSupported("immersive-ar").then((supported) => {
+      ar_supported = supported;
+      
+      init();
+      animate();
+    });
+  } else {
+    console.log("WebXR not supported");
+  }
+
+  function sessionStart() {
+    planeFound = false;
+    //show #tracking-prompt
+    // document.getElementById("tracking-prompt").style.display = "block";
+  }
+
+  function init() {
+    container = document.createElement("div");
+    document.body.appendChild(container);
+
+    scene = new THREE.Scene();
+
+    camera = new THREE.PerspectiveCamera(
+      35,
+      window.innerWidth / window.innerHeight,
+      0.01,
+      20
+    );
+
+    let aspect = window.innerWidth / window.innerHeight;
+    const fator_tela = 0.2 / Math.min(aspect,1);
+
+    console.log('aspect', aspect, fator_tela);
+
+    camera.position.set(0.0, 2*fator_tela, 3*fator_tela);
+
+    camera.lookAt(0, 0, 0);
+
+    const light = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 1);
+    light.position.set(0.5, 1, 0.25);
+    scene.add(light);
+
+    renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    container.appendChild(renderer.domElement);
+
+    if (ar_supported) {
+      renderer.xr.enabled = true;
+
+      renderer.xr.addEventListener("sessionstart", sessionStart);
+
+      document.body.appendChild(
+        ARButtonCustom.createButton(renderer, {
+          requiredFeatures: ["local", "hit-test", "dom-overlay"],
+          domOverlay: { root: document.querySelector("#overlay") },
+        })
+      );
+    }
+
+    function create_note(reticle_matrix = null) {
+      if (!noteGltf) {
+        Bugfender.log("noteGltf is null");
+        return;
+      }
+
+      const mesh = noteGltf.scene.clone();
+
+      if (reticle_matrix) {
+        reticle_matrix.decompose(mesh.position, mesh.quaternion, mesh.scale);
+      } else {
+        mesh.position.y -= 0.1;
+      }
+      
+      scene.add(mesh);
+    }
+
+    function onSelect(ar_mode = true) {
+      if (noteGltf.scene && (! ar_mode || reticle.visible)) {
+        create_note(reticle.matrix);
+      }
+    }
+
+    const loader = new GLTFLoader();
+
+    loader.load("../examples/glb/Nitro-V_parallax_menor.glb", (gltf) => {
+      noteGltf = gltf;
+
+      create_note();
+    });
+
+    controller = renderer.xr.getController(0);
+    controller.addEventListener("select", onSelect);
+    scene.add(controller);
+    
+    console.log("Iniciando OrbitControls sem AR");
+    controls = new OrbitControls(camera, renderer.domElement);
+
+    reticle = new THREE.Mesh(
+      new THREE.RingGeometry(0.15, 0.2, 32).rotateX(-Math.PI / 2),
+      new THREE.MeshBasicMaterial()
+    );
+    reticle.matrixAutoUpdate = false;
+    reticle.visible = false;
+    scene.add(reticle);
+
+    window.addEventListener("resize", onWindowResize);
+  }
+
+  function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize(window.innerWidth, window.innerHeight);
+  }
+
+  function animate() {
+    renderer.setAnimationLoop(render);
+  }
+
+  function render(timestamp, frame) {
+    // Bugfender.log("frame", frame);
+
+    if (frame) {
+      const referenceSpace = renderer.xr.getReferenceSpace();
+      const session = renderer.xr.getSession();
+
+      if (hitTestSourceRequested === false) {
+        session.requestReferenceSpace("viewer").then(function (referenceSpace) {
+          session
+            .requestHitTestSource({ space: referenceSpace })
+            .then(function (source) {
+              hitTestSource = source;
+            });
+        });
+
+        session.addEventListener("end", function () {
+          hitTestSourceRequested = false;
+          hitTestSource = null;
+        });
+
+        hitTestSourceRequested = true;
+      }
+
+      if (hitTestSource) {
+        const hitTestResults = frame.getHitTestResults(hitTestSource);
+
+        if (hitTestResults.length) {
+          if (!planeFound) {
+            planeFound = true;
+            //hide #tracking-prompt
+            // document.getElementById("tracking-prompt").style.display = "none";
+            // document.getElementById("instructions").style.display = "flex";
+          }
+          const hit = hitTestResults[0];
+
+          reticle.visible = true;
+          reticle.matrix.fromArray(hit.getPose(referenceSpace).transform.matrix);
+        } else {
+          reticle.visible = false;
+        }
+      }
+    }
+
+    renderer.render(scene, camera);
+  }
+});
